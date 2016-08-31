Fixed a bug and moved the scaling out of he config class.

biot_savart_visualization/field_generator.py
@@ -17,10 +17,10 @@ def CosCurrent(config):
 	x = np.arange(START, END, config.res.step)
 	y = ((config.dim.startHeight - config.dim.endHeight)/2) * np.sin(x * RADIANS)
 	z = [config.dim.startLength for k in range(len(x))] # Idk, I mean this seems fine right? Every coordiange will have 1 as the depth. Eh.
-	#The u,v,w components don't really matter, but they should be to an apporproiate scale.
+	#The u,v,w components don't really matter for the calculation, but for displaying they should be to an apporproiate scale.
 	
-	u = [config.dim.width/0.05 for k in range(len(z))]   #TODO
-	v = [config.dim.height/0.05 for k in range(len(z))] #TODO
-	w = [config.dim.startLength for k in range(len(z))]
+	u = [config.dim.width/0.05 for k in range(len(x))]   #TODO
+	v = [config.dim.height/0.05 for k in range(len(x))] #TODO
+	w = [config.dim.startLength for k in range(len(x))]
 	
-	return vector_field.VectorField(x,y,z,u,v,w).scale(config.scale)
+	return vector_field.VectorField(x,y,z,u,v,w)

biot_savart_visualization/physics_calc.py
@@ -32,7 +32,6 @@ def Biot_Savart(B_config,J_field):
 
 	dim = B_config.dim
 	res = B_config.res
-	scale = B_config.scale
 
 	x = []
 	y = []
@@ -41,19 +40,18 @@ def Biot_Savart(B_config,J_field):
 	u = []
 	v = []
 	w = []
-
+	
 	for _x in np.arange(dim.startWidth,dim.endWidth,res.step):
 		for _y in np.arange(dim.startHeight,dim.endHeight,res.step):
 			for _z in np.arange(dim.startLength,dim.endLength,res.step):
-				x.append(_x)
-				y.append(_y)
-				z.append(_z)
 	
 				if (_x,_y,_z) not in J_field.keys:
+					x.append(_x)
+					y.append(_y)
+					z.append(_z)
 	
 					_u,_v,_w = _dB_calc(J_field, _x, _y, _z)
 					u.append(_u)
 					v.append(_v)
 					w.append(_w)
-
 	return vector_field.VectorField(x,y,z,u,v,w)